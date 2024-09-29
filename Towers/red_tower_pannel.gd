df5c5a976a97035d2ccacb3a25b3a9515b7f2aa2
JoshuaxLasley <jlasley@ftstudent.org>
2024-09-29 16:05:10 -0400

    Done with video 4

---
+++ Towers/red_tower_pannel.gd
@@ -0,0 +1,35 @@
+extends Panel
+
+@export var tower = preload("res://Towers/red_bullet_tower.tscn")
+var currTile
+
+func _on_gui_input(event: InputEvent):
+	var tempTower = tower.instantiate()
+	if event is InputEventMouseButton and event.button_mask == 1:
+		#left Click Down
+		add_child(tempTower)
+		tempTower.global_position = event.global_position
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+		
+		tempTower.scale = Vector2(0.32, 0.32)
+		
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		#left click down drag
+		get_child(1).global_position = event.global_position
+		
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		#left click up
+		if event.global_position.x >= 2944:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
+		else:
+			if get_child_count(1) > 1:
+				get_child(1).queue_free()
+			var path = get_tree().get_root().get_node("Main/Towers")
+		
+			path.add_child(tempTower)
+			tempTower.global_position = event.global_position
+			tempTower.get_node("Area").hide()
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
