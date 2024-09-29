063f0b34e68ad97a82ecba2cacdf11022c72e734
JoshuaxLasley <jlasley@ftstudent.org>
2024-09-29 16:50:33 -0400

    stuff
    We need to fix tile map

--- Towers/red_tower_pannel.gd
+++ Towers/red_tower_pannel.gd
@@ -15,7 +15,16 @@ func _on_gui_input(event: InputEvent):
 		
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		#left click down drag
-		get_child(1).global_position = event.global_position
+		if get_child_count() > 1:
+			get_child(1).global_position = event.global_position
+		
+		var mapPath = get_tree().get_root().get_node("Main/TileMap")
+		var tile = mapPath.local_to_map(get_global_mouse_position())
+		currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+		if (currTile == Vector2i(4,5)):
+			get_child(1).get_node("Area").modulate = Color(0,255,0)
+		else:
+			get_child(1).get_node("Area").modulate = Color(255,255,255)
 		
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#left click up
@@ -25,11 +34,12 @@ func _on_gui_input(event: InputEvent):
 		else:
 			if get_child_count(1) > 1:
 				get_child(1).queue_free()
-			var path = get_tree().get_root().get_node("Main/Towers")
-		
-			path.add_child(tempTower)
-			tempTower.global_position = event.global_position
-			tempTower.get_node("Area").hide()
+			if currTile == Vector2i(4,5):
+				var path = get_tree().get_root().get_node("Main/Towers")
+			
+				path.add_child(tempTower)
+				tempTower.global_position = event.global_position
+				tempTower.get_node("Area").hide()
 	else:
 		if get_child_count() > 1:
 			get_child(1).queue_free()
