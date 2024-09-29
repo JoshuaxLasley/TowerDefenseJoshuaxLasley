fc4f37d8e2197f96d0d21f9c813c68594446ac47
JoshuaxLasley <jlasley@ftstudent.org>
2024-09-29 13:54:12 -0400

    Soldier A shoots at targets

--- Towers/red_bullet_tower.gd
+++ Towers/red_bullet_tower.gd
@@ -12,13 +12,26 @@ func _on_tower_body_entered(body:Node2D) -> void:
 		var tempArray = []
 		currTargets = get_node("Tower").get_overlapping_bodies()
 
-		print(currTargets)
-
+		for i in currTargets:
+			if "Soldier" in i.name:
+				tempArray.append(i)
+		var currTarget = null
+		
+		for i in tempArray:
+			if currTarget == null:
+				currTarget = i.get_node("../")
+			else:
+				if i.get_parent().get_progress() > currTarget.get_progress():
+					currTarget = i.get_node("../")
+		curr = currTarget
+		var pathName = currTarget.get_parent().name
+		
+		var tempBullet = Bullet.instantiate()
+		tempBullet.pathName = pathName
+		tempBullet.bulletDamage = bulletDamage
+		get_node("BulletContainer").add_child(tempBullet)
+		tempBullet.global_position = $Aim.global_position
 
 
 func _on_tower_body_exited(body:Node2D) -> void:
 	pass # Replace with function body.
-
-
-
-
